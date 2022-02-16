44812edb7812e97d7860aa1996ab879cc2e03312
tienph <tienph@fabbi.io>
2022-02-16 15:28:36 +0700

    websocket-loadbalancer with nginx

--- src/main/java/com/example/websockettest/GreetingController.java
+++ src/main/java/com/example/websockettest/GreetingController.java
@@ -1,13 +1,27 @@
 package com.example.websockettest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.util.HtmlUtils;
 
-@Controller public class GreetingController {
+@Controller
+public class GreetingController {
+
+        @Autowired
+        Environment environment;
+
+        @GetMapping("/sayHello")
+        public String sayHello() {
+                String port = environment.getProperty("server.port");
+                System.out.println("Hello world!!! from "+port);
+                return "";
+        }
 
         @Autowired private SimpMessagingTemplate template;
 
@@ -18,10 +32,13 @@ import org.springframework.web.util.HtmlUtils;
         @MessageMapping("/download") @SendTo("/topic/download") public Greeting download(String str) {
                 Downloader downloader = new Downloader(this);
                 new Thread(downloader).start();
-                return new Greeting("Hello, waiting for downloading, pls!!!");
+                String port = environment.getProperty("server.port");
+                return new Greeting("Hello, waiting for downloading, pls!!! from: " + port);
         }
 
         public void fireDownload() {
+                String port = environment.getProperty("server.port");
+                System.out.println("Downloaded!!! from "+port);
                 this.template.convertAndSend("/topic/download-finish", new Greeting("/employee.xlsx"));
         }
 }
